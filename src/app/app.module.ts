5c45907675f041dd9d83949c852218854ea3db4f
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-05-24 11:22:46 -0300

    Footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 //Angular material imports:
 import {MatToolbarModule} from '@angular/material/toolbar';
-
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
